c14558ea6510a5ae5e9254e6f92a44267486824f
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-06-15 09:44:20 +0900

    feat(figure): support filled figure

--- src/fig/RectangleFigure.java
+++ src/fig/RectangleFigure.java
@@ -2,12 +2,19 @@ package fig;
 
 import java.awt.*;
 
-public class RectangleFigure extends Figure {
+public class RectangleFigure extends FillableFigure {
     public RectangleFigure(int x, int y, int w, int h, Color c) {
         super(x, y, w, h, c);
     }
+    public RectangleFigure(int x, int y, int w, int h, Color c, boolean isFilled) {
+        super(x, y, w, h, c, isFilled);
+    }
     public void draw(Graphics g) {
         g.setColor(color);
-        g.drawRect(x, y, width, height);
+        if (isFilled) {
+            g.fillRect(x, y, width, height);
+        } else {
+            g.drawRect(x, y, width, height);
+        }
     }
 }
