aa4f7c6fbc7ebf45e7a1f687f869418d22871612
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-06-19 18:08:16 +0900

    refactor(status view): remove view data

--- src/view/StatusView.java
+++ src/view/StatusView.java
@@ -1,48 +1,27 @@
 package view;
 
-import event.CommandEvent;
-import event.CommandListener;
-import model.CommandProcessor;
+import command.ModeEnum;
+import controller.CommandController;
+import event.ModeEvent;
+import event.ModeListener;
 
 import javax.swing.*;
-import java.util.HashMap;
 
-public class StatusView extends JPanel implements CommandListener {
+public class StatusView extends JPanel implements ModeListener {
     protected final JLabel modeLabel;
-    protected final CommandProcessor cmdProcessor;
-    protected final HashMap<CommandProcessor.ModeEnum, String> modeNames;
-    public StatusView(CommandProcessor cp) {
-        cmdProcessor = cp;
-        cmdProcessor.addListener(this);
-        modeNames = createModeNames();
+    protected final CommandController cmdProcessor;
 
-        modeLabel = new JLabel(modeNames.get(CommandProcessor.ModeEnum.NORMAL));
+    public StatusView(CommandController cp) {
+        cmdProcessor = cp;
+        cmdProcessor.addModeListener(this);
+
+        modeLabel = new JLabel(ModeEnum.NORMAL.name);
         setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
         add(new JLabel("mode: "));
         add(modeLabel);
     }
-    public void modeChanged(CommandEvent e) {
-        modeLabel.setText(modeNames.get(cmdProcessor.getMode()));
-    }
 
-    protected static HashMap<CommandProcessor.ModeEnum, String> createModeNames() {
-        HashMap<CommandProcessor.ModeEnum, String> modeNames = new HashMap<>();
-        modeNames.put(
-                CommandProcessor.ModeEnum.NORMAL,
-                CommandViewData.createNormalModeData().name
-        );
-        modeNames.put(
-                CommandProcessor.ModeEnum.FILE,
-                CommandViewData.createFileModeData().name
-        );
-        modeNames.put(
-                CommandProcessor.ModeEnum.COLOR,
-                CommandViewData.createColorModeData().name
-        );
-        modeNames.put(
-                CommandProcessor.ModeEnum.BRUSH,
-                CommandViewData.createBrushModeData().name
-        );
-        return modeNames;
+    public void modeChanged(ModeEvent e) {
+        modeLabel.setText(cmdProcessor.getMode().name);
     }
 }
