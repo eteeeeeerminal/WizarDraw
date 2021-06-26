cdcd8a03b5c3f89fe8d2dcd2133ca8d96e759ce9
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-06-26 20:40:27 +0900

    docs(canvas event): add javadoc

--- src/event/CanvasEvent.java
+++ src/event/CanvasEvent.java
@@ -4,6 +4,10 @@ import model.Canvas;
 
 import java.util.EventObject;
 
+/**
+ * This class doesn't have any roles yet.
+ * See {@link CanvasListener}
+ */
 public class CanvasEvent extends EventObject {
     public CanvasEvent(Canvas source) {
         super(source);
--- src/event/CanvasListener.java
+++ src/event/CanvasListener.java
@@ -2,6 +2,14 @@ package event;
 
 import java.util.EventListener;
 
+/**
+ * Listen to changes of {@link model.Canvas} object
+ * See {@link view.CanvasView}
+ */
 public interface CanvasListener extends EventListener {
+    /**
+     * Any changes of {@link model.Canvas} object trigger canvasUpdated.
+     * @param e
+     */
     void canvasUpdated(CanvasEvent e);
 }
