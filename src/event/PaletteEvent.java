3823bc8c17b45a2b84ca35dc3a77c5f86cdc6108
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-06-26 21:09:55 +0900

    docs(palette event): add javadoc

--- src/event/PaletteEvent.java
+++ src/event/PaletteEvent.java
@@ -4,6 +4,10 @@ import model.PaletteAndBrush;
 
 import java.util.EventObject;
 
+/**
+ * Used in PaletteListener. This class doesn't have any roles yet.
+ * See {@link PaletteListener}
+ */
 public class PaletteEvent extends EventObject {
     public PaletteEvent(PaletteAndBrush source) {
         super(source);
--- src/event/PaletteListener.java
+++ src/event/PaletteListener.java
@@ -2,6 +2,14 @@ package event;
 
 import java.util.EventListener;
 
+/**
+ * Listen to change of {@link model.PaletteAndBrush} object.
+ * See {@link view.PaletteView}
+ */
 public interface PaletteListener extends EventListener {
+    /**
+     * Any changes of {@link model.PaletteAndBrush} object trigger paletteUpdated.
+     * @param e
+     */
     void paletteUpdated(PaletteEvent e);
 }
