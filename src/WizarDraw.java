b520e4c25b93277c22d3a5385140532a412bc607
えてみなる <47494433+eteeeeeerminal@users.noreply.github.com>
2021-06-26 14:43:01 +0900

    docs(main class): add some comments and javadoc comments

--- src/WizarDraw.java
+++ src/WizarDraw.java
@@ -5,42 +5,69 @@ import model.*;
 import controller.*;
 import view.*;
 
-// Main class
+/**
+ * Main Class of WizarDraw
+ * <p>
+ *     This class combines Model, View, and Controller, then constructs a main window.
+ *     This class isn't called from other class.
+ */
 class WizarDraw extends JFrame {
+    /**
+     * Set the title for the main window frame
+     */
     public static final String appName = "WizarDraw";
+    /**
+     * Main window width initialized by this value
+     */
     public static final int initialWidth = 1280;
+    /**
+     * Main window height initialized by this value
+     */
     public static final int initialHeight = 720;
 
+    /**
+     * Initializes and Constructs Model, View, and Controller
+     */
     public WizarDraw() {
+        // initialize Model and Controller
         PaletteAndBrush palette = new PaletteAndBrush();
         Canvas canvas = new Canvas();
         DrawModel model = new DrawModel(palette, canvas);
         CommandController commandCtrl = new CommandController();
         commandCtrl.addCommandListener(model);
-        this.addKeyListener(commandCtrl);
+        addKeyListener(commandCtrl);
 
+        // initialize Canvas View
         CanvasView canvasView = new CanvasView(canvas);
         canvasView.addMouseListener(commandCtrl);
         canvasView.addMouseMotionListener(commandCtrl);
-        this.add(canvasView, BorderLayout.CENTER);
+        add(canvasView, BorderLayout.CENTER);
 
+        // initialize Tool Bar
         JPanel toolBar = new JPanel();
         toolBar.setLayout(new BoxLayout(toolBar, BoxLayout.X_AXIS));
         toolBar.add(new PaletteView(palette));
         StatusView status = new StatusView();
         commandCtrl.addModeListener(status);
         toolBar.add(status);
-        this.add(toolBar, BorderLayout.NORTH);
+        add(toolBar, BorderLayout.NORTH);
 
+        // initialize Navigator View
         CommandNavigator navi = new CommandNavigator();
         commandCtrl.addModeListener(navi);
-        this.add(navi, BorderLayout.WEST);
+        add(navi, BorderLayout.WEST);
 
-        this.setTitle(appName);
-        this.setSize(initialWidth, initialHeight);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
+        // initialize window
+        setTitle(appName);
+        setSize(initialWidth, initialHeight);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
+
+    /**
+     * Constructs WizarDraw
+     * @param args isn't used
+     */
     public static void main(String[] args) {
         new WizarDraw();
     }
